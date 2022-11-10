f5da06dcc6350a26d2373fb28b0eb000c45cb0ce
xiaoxianxiong <1661214933@qq.com>
2022-11-10 17:18:31 +0800

    rabbitmq 简单完成

--- rabbitmq-delay/src/main/java/com/majorco/delay/RabbitDelayTask.java
+++ rabbitmq-delay/src/main/java/com/majorco/delay/RabbitDelayTask.java
@@ -1,12 +1,14 @@
 package com.majorco.delay;
 
 import java.io.Serializable;
+import lombok.Builder;
 import lombok.Data;
 
 /**
  * @author xxxiao
  **/
 @Data
+@Builder
 public abstract class RabbitDelayTask implements Serializable, Runnable {
 
   private static final long serialVersionUID = -6881959415222743107L;
